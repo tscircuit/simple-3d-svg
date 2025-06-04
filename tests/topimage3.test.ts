98d38502b6201d8439461f1276af51f9f67ce363
seveibar <seveibar@gmail.com>
2025-06-04 16:55:30 -0700

    better perspective example

--- tests/topimage3.test.ts
+++ tests/topimage3.test.ts
@@ -6,16 +6,20 @@ test("topimage3", () => {
   const svg = renderScene({
     boxes: [
       {
-        center: { x: 0, y: 0, z: 5 },
+        center: { x: 0, y: 0, z: 0 },
         size: { x: 2, y: 2, z: 2 },
         color: "gray",
         faceImages: {
           top: CHECKER_10x10,
         },
-        projectionSubdivision: 4,
+        projectionSubdivision: 10,
       },
     ],
-    camera: { position: { x: 1, y: 3, z: 0 }, lookAt: { x: 0, y: 0, z: 5 } },
+    camera: {
+      position: { x: 0, y: 1.3, z: 1.3 },
+      lookAt: { x: 0, y: -1, z: 0 },
+      focalLength: 0.4,
+    },
   })
 
   expect(svg).toMatchSvgSnapshot(import.meta.path)
