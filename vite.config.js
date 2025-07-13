96a36eb66e0c495e33e571d12cd01d9cf75edc4f
seveibar <seveibar@gmail.com>
2025-07-13 08:34:38 -0700

    format

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite"
 
 export default defineConfig({
   resolve: {
     alias: {
-      lib: '/lib',
-      tests: '/tests',
+      lib: "/lib",
+      tests: "/tests",
     },
   },
 })
