7f93277b29859db49907c41cff5319dd51138784
Harshit Pal <harshit13082006@gmail.com>
2025-09-28 09:20:25 +0530

    Optimize SVG rendering performance (issue #56)

--- lib/render-svg.ts
+++ lib/render-svg.ts
@@ -4,7 +4,13 @@ import { buildRenderElements } from "./render-elements"
 import { sub, cross, dot, len, norm, add, scale } from "./vec3"
 
 function fmt(n: number): string {
-  return Math.trunc(n + 0.5) + "";
+  // Only check bounds for obviously large numbers
+  if (n > 1000000 || n < -1000000) {
+    if (n > 2147483647 || n < -2147483648) {
+      return Math.round(n) + "";
+    }
+  }
+  return ((n + 0.5) | 0) + "";
 }
 
 export async function renderScene(
